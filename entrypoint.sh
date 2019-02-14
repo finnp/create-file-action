97bc8e67dceef1de703c7ec0f79a2cf3eefa0ba1
Finn Pauls <derfinn@gmail.com>
2019-02-15 00:19:20 +0100

    Fix bash script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 
 if [[ -z "${FILE_BASE64}" ]]; then
   echo "Using FILE_DATA to write to $FILE_NAME"
   echo $FILE_DATA > "$FILE_NAME"
 else
   echo "Using FILE_BASE64 to write to $FILE_NAME"
-  echo $FILE_BASE64 | base64 -D > $FILE_NAME
+  echo $FILE_BASE64 | base64 --decode > $FILE_NAME
 fi

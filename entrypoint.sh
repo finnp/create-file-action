9a3ad6c1d5413bc28f5838223183891f4862dbde
Finn Pauls <derfinn@gmail.com>
2019-02-15 00:04:38 +0100

    Add BASE64

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
-echo $FILE_DATA > $FILE_NAME
+if [[ -z "${FILE_BASE64}" ]]; then
+  echo "Using FILE_DATA to write to $FILE_NAME"
+  echo $FILE_DATA > "$FILE_NAME"
+else
+  echo "Using FILE_BASE64 to write to $FILE_NAME"
+  echo $FILE_BASE64 | base64 -D > $FILE_NAME
+fi

67c3237f6d3f57874a0109d496367e99802f81f3
Angel Lacret <alacret@gmail.com>
2023-06-19 09:47:14 -0400

    Update entrypoint.sh
    Support -e in echo

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ then
 fi
 if [[ -z "${FILE_BASE64}" ]]; then
   echo "Using FILE_DATA to write to $FILE_NAME"
-  echo $FILE_DATA > "$FILE_NAME"
+  echo -e $FILE_DATA > "$FILE_NAME"
 else
   echo "Using FILE_BASE64 to write to $FILE_NAME"
   echo $FILE_BASE64 | base64 --decode > $FILE_NAME
